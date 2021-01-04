c00876bae755924edcec56b9646157c183b94563
Your Name <you@example.com>
2021-01-04 12:48:47 +0600

    Committed the base Xml for project

--- app/src/main/java/com/example/firebaseproject/MainActivity.java
+++ app/src/main/java/com/example/firebaseproject/MainActivity.java
@@ -3,11 +3,8 @@ package com.example.firebaseproject;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-
-
-
 public class MainActivity extends AppCompatActivity {
-    String n_Is1;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
