793760af457501781884ceca4ba9cc60173402ff
Your Name <you@example.com>
2021-01-04 11:56:47 +0600

    My project initial commit

---
+++ app/src/main/java/com/example/firebaseproject/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.firebaseproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+
+
+public class MainActivity extends AppCompatActivity {
+    String n_Is1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+}}
